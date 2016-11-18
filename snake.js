7c35b7f15265105da401a16ca8435968bee3a296
Karma Shenphen Dorje <adam.pol1234@gmail.com>
2016-11-18 17:38:12 +0100

    Added basic files

---
+++ snake.js
@@ -0,0 +1,511 @@
+/*  CONSTANCES   */
+
+var ROWS_NUM = 20;
+var COL_NUM = 30;
+var PIX_SIZE = 16;
+var LEFT = 37, UP = 38, RIGHT = 39, DOWN = 40;
+var APPLE_AMOUNT = 3, APPLE_LIMIT = 10;
+function Vector(x, y){
+	this.x = x;
+	this.y = y;
+}
+
+
+
+Vector.prototype.copy = function () {
+	return new Vector(this.x, this.y);
+};
+
+Vector.prototype.isSame = function (vector) {
+	return this.x == vector.x && this.y == vector.y;
+};
+
+Vector.prototype.isInsideArray = function (arr) {
+	for(var i = 0; i < arr.length; i++) {
+		if(this.isSame(arr[i])) return true;
+	}
+	return false;
+};
+Vector.prototype.indexOfArray = function (arr) {
+	for(var i = 0; i < arr.length; i++) {
+		if(this.isSame(arr[i])) return i;
+	}
+	return null;
+};
+
+function componentToHex(c) {
+	var hex = c.toString(16);
+	return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+	return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+
+function Snake(){
+	var centerX = Math.floor((COL_NUM-1)/2);
+	var centerY = Math.floor((ROWS_NUM-1)/2);
+	this.alive = false;
+	this.tail = [new Vector(centerX    , centerY), new Vector(centerX - 1, centerY),
+				 new Vector(centerX - 2, centerY)];
+	this.direction = RIGHT;
+	// this.refreshTime = 150;
+	this.throughWalls = true;
+	this.needless = new Vector(-1, -1); //Vector of last part which is not needed after turn
+							// (useful in CanvasDisplay.prototype.draw to clear only one "pixel")
+	this.apples = [];
+	this.eated = [];
+	for(var i  = 0; i < APPLE_AMOUNT; ++i)
+		this.randomApple();
+}
+
+Snake.prototype.randomApple = function () {
+	var apple = new Vector(Math.floor(Math.random() * COL_NUM), Math.floor(Math.random() * ROWS_NUM));
+	var length = this.apples.length;
+	if (length < COL_NUM * ROWS_NUM - this.tail.length - this.eated.length	&& length < APPLE_LIMIT) {
+			if (!apple.isInsideArray(this.tail) && !apple.isInsideArray(this.apples))
+				this.apples.push(apple);
+			else this.randomApple();
+		}
+};
+
+
+Snake.prototype.update = function(){
+	var newHead = this.tail[0].copy();
+	if(this.direction == RIGHT) newHead.x += 1;
+	else if(this.direction == LEFT) newHead.x -= 1;
+	else if(this.direction == UP) newHead.y -= 1;
+	else if(this.direction == DOWN) newHead.y += 1;
+	this.needless = this.tail.pop();
+	this.tail.unshift(newHead);
+
+	//Winning
+	if(newHead.isSame(this.needless)) {
+		//TODO auto going and disabling keys
+		var shown = true;
+		if(shown)console.log("koniec gry");
+	}
+
+	//Collisions
+	var head = this.tail[0];
+
+	if(this.throughWalls === true){
+		if(head.x >= COL_NUM) head.x = 0;
+		else if(head.x < 0) head.x = COL_NUM - 1;
+		if(head.y >= ROWS_NUM) head.y = 0;
+		else if(head.y < 0) head.y = ROWS_NUM - 1;
+	}
+	if(!this.throughWalls &&(head.x >= COL_NUM || head.x < 0 || head.y >= ROWS_NUM || head.y < 0)) this.alive = false;
+
+	for (var i = 1; i < this.tail.length; ++i) {
+		if (this.tail[0].x == this.tail[i].x && this.tail[0].y == this.tail[i].y)
+			this.alive = false;
+		}
+
+	//Eating event
+	var index = head.indexOfArray(this.apples);
+	if(index !== null){
+		var apple = this.apples.splice(index, 1)[0];
+		this.eated.push(apple);
+		this.randomApple();
+	}
+
+
+	for (i = 0; i < this.eated.length ; ++i){
+		var elem = this.eated[i];
+		if (!elem.isInsideArray(this.tail)) {
+			this.tail.push(this.eated.splice(i, 1)[0]);
+		}
+	}
+
+};
+
+function CanvasDisplay(canvas) {
+	this.cx = canvas.getContext("2d");
+	this.width = COL_NUM * PIX_SIZE;
+	this.height = ROWS_NUM * PIX_SIZE;
+
+	this.grid = null; //Of course we don't have to make a grid and we can just transform
+															//the elements, but I find that way not consuming so much countings
+	this.bgColor = "green";
+	this.loaded = false;
+
+	//Setup
+	this.vect = {								// VECTORS to cut the proper element from grid image
+		head_up: new Vector(0, 0),
+		head_right: new Vector(0, PIX_SIZE),
+		head_down: new Vector(0, 2 * PIX_SIZE),
+		head_left: new Vector(0, 3 * PIX_SIZE),
+		ul: new Vector(PIX_SIZE, 0), 				//shortcuts stands for up-left, up-right, down-left & down-right
+		ur: new Vector(PIX_SIZE, PIX_SIZE),
+		dl: new Vector(PIX_SIZE, 2 * PIX_SIZE),
+		dr: new Vector(PIX_SIZE, 3 * PIX_SIZE),
+		norm_v: new Vector(2 * PIX_SIZE, 0),		//normal body horizontal & vertical
+		norm_h: new Vector(2 * PIX_SIZE, PIX_SIZE),
+		tail_up: new Vector(3 * PIX_SIZE, 0),
+		tail_right: new Vector(3 * PIX_SIZE, PIX_SIZE),
+		tail_down: new Vector(3 * PIX_SIZE, 2 * PIX_SIZE),
+		tail_left: new Vector(3 * PIX_SIZE, 3 * PIX_SIZE),
+		apple: new Vector(2 * PIX_SIZE, 3 * PIX_SIZE),
+		eated: new Vector(2 * PIX_SIZE, 2 * PIX_SIZE)
+	};
+
+	canvas.width = this.width;
+	canvas.height = this.height;
+}
+
+CanvasDisplay.prototype.drawElement = function (displayVector, tailVector) {
+	this.cx.drawImage(this.grid, displayVector.x, displayVector.y, PIX_SIZE, PIX_SIZE,
+		tailVector.x * PIX_SIZE, tailVector.y * PIX_SIZE, PIX_SIZE, PIX_SIZE);
+};
+
+
+CanvasDisplay.prototype.drawElementIndex = function (tailArray, index) {
+		var relation = "";																	//That works ONLY on grid at least 3x3 pixel sizes;
+		var prev = tailArray[index - 1], mid = tailArray[index], next = tailArray[index + 1];
+		if(next) {
+			if (prev.x == next.x) relation = "norm_v";
+			else if (prev.y == next.y) relation = "norm_h";
+			else if (prev.x == mid.x) {
+				if (prev.y > mid.y && Math.abs(prev.y - mid.y) <= 1 ||
+					prev.y < mid.y && Math.abs(prev.y - mid.y) > 1) relation += "d";
+				else relation += "u";
+				if (mid.x > next.x && Math.abs(mid.x - next.x) <= 1 ||
+					mid.x < next.x && Math.abs(mid.x - next.x) > 1) relation += 'l';
+				else relation += 'r';
+			}
+			else {
+				if (mid.y > next.y && Math.abs(mid.y - next.y) <= 1 ||
+					mid.y < next.y && Math.abs(mid.y - next.y) > 1) relation += "u";
+				else relation += "d";
+				if (prev.x > mid.x && Math.abs(mid.x - prev.x) <= 1 ||
+					prev.x < mid.x && Math.abs(mid.x - prev.x) > 1) relation += 'r';
+				else relation += 'l';
+			}
+		}
+		else {
+			if(mid.x == prev.x) {
+				if(mid.y > prev.y && Math.abs(mid.y - prev.y) <= 1 ||
+					mid.y < prev.y && Math.abs(mid.y - prev.y) > 1) relation = "tail_up";
+				else relation = "tail_down";
+			}
+			else {
+				if(mid.x > prev.x && Math.abs(mid.x - prev.x) <= 1 ||
+					mid.x < prev.x && Math.abs(mid.x - prev.x) > 1) relation = "tail_left";
+				else relation = "tail_right";
+			}
+		}
+		var vector = this.vect[relation];
+		if(vector) this.drawElement(vector, mid);
+		else return null;
+};
+
+
+CanvasDisplay.prototype.firstDraw = function (snake) {
+	this.cx.fillStyle = this.bgColor;
+	this.cx.fillRect(0, 0, this.width, this.height);
+	this.drawElement(this.vect.head_right, snake.tail[0]);
+	for(var i = 1; i < snake.tail.length; ++i)
+		this.drawElementIndex(snake.tail, i);
+
+	this.drawApples(snake);
+};
+CanvasDisplay.prototype.clearEnd = function (lastElement) {
+	this.cx.fillStyle = this.bgColor;
+	this.cx.fillRect(lastElement.x * PIX_SIZE, lastElement.y * PIX_SIZE, PIX_SIZE, PIX_SIZE);
+};
+
+CanvasDisplay.prototype.drawApples = function (snake) {
+	var apples = snake.apples;
+	for (var i = 0; i < apples.length; i++) {
+		this.drawElement(this.vect.apple, apples[i]);
+	}
+};
+
+CanvasDisplay.prototype.draw = function (snake) {
+
+	// this.cx.translate(element.x * PIX_SIZE + PIX_SIZE / 2, element.y * PIX_SIZE + PIX_SIZE / 2);
+	// this.cx.rotate(angle);
+
+
+	this.clearEnd(snake.needless);	//CLEAR LAST ELEMENT
+	this.drawElementIndex(snake.tail, 1);	//REDRAW 2nd ELEMENT
+	snake.eated.forEach(function (elem) {
+		this.drawElement(this.vect.eated, elem);
+	}, this);
+
+	this.drawElementIndex(snake.tail, snake.tail.length - 1);	//DRAW LAST ELEMENT
+	this.drawApples(snake);
+
+	var head = snake.tail[0];
+	var head_vect = null;
+	if(snake.direction == RIGHT) head_vect = this.vect.head_right;
+	else if(snake.direction == LEFT) head_vect = this.vect.head_left;
+	else if(snake.direction == DOWN) head_vect = this.vect.head_down;
+	else head_vect = this.vect.head_up;
+	this.drawElement(head_vect, head);	//DRAW NEW HEAD
+
+};
+
+var Menu = function (id) {
+	var name = id ? id : "menu";
+	this.node = document.getElementById(name);
+	this.hidden = false;
+	this.buttons = [];
+	var buttons = this.node.childNodes;
+	for(var i = 0; i < buttons.length; ++i) {
+		if(buttons[i].nodeType == Node.ELEMENT_NODE) {
+			this.buttons.push(buttons[i]);
+		}
+	}
+	if(name == "menu") this.continue = document.getElementById("continue-btn");
+	this.saveButton = document.getElementById("save-settings");
+
+	this.settings = false; //if settings showed or not
+
+
+	this.switchButtons(true);
+
+
+};
+
+
+Menu.prototype.switchButtons = function (value) {
+	var display = value ? "block" : "none";
+	for(var i = 0; i < this.buttons.length; ++i) {
+		this.buttons[i].disabled = !value;
+		this.buttons[i].style.display = display;
+	}
+};
+
+Menu.prototype.enableContinue = function (value) {
+	this.continue.disabled = !value;
+};
+
+Menu.prototype.show = function () {
+	this.node.className = "show";
+	this.hidden = false;
+	this.switchButtons(true);
+};
+
+Menu.prototype.hide = function () {
+	this.node.className = "hidden";
+	this.hidden = true;
+	this.switchButtons(false);
+};
+
+
+Menu.prototype.toggleView = function () {
+	if (this.hidden) {
+		this.node.className = "show";
+	}
+	else {
+		this.node.className = "hidden";
+	}
+	this.hidden = !this.hidden;
+	return !this.hidden; //Information for game if pause or not
+};
+
+Menu.prototype.toggleSettings = function () {
+	if (this.settings) {
+		this.saveButton.disabled = true;
+		this.node.className = "show settings_off";
+		this.settings = false;
+	}
+	else {
+		this.saveButton.disabled = false;
+		this.node.className = "settings_on";
+		this.settings = true;
+	}
+};
+
+
+
+function Game(snake, display) {
+	this.snake = snake;
+	this.display = display;
+
+	this.paused = false;
+	this.stepTime = 100;
+	this.wasStep = false;   //Prevents making quick change to opposite direction (used in Game.prototype.run())
+	 						// (e.g. direction now is RIGHT and we press right after another UP and LEFT
+
+
+
+
+	var pic = new Image(); //document.getElementById("snake-grid-norm");
+	pic.src = "img/snake_grid_bg.png";
+	var self = this;
+	pic.addEventListener("load", function () {
+		self.display.grid = pic;
+		self.display.cx.drawImage(self.display.grid, 2 * PIX_SIZE, 2 * PIX_SIZE, 1, 1, 0, 0, 1, 1); //Setting background from grid image
+		var color = self.display.cx.getImageData(0, 0, 1, 1).data;
+		self.display.bgColor = rgbToHex(color[0], color[1], color[2]);
+		self.display.firstDraw(self.snake);
+	}, false); //Synchronic
+
+	this.menu = new Menu();
+	this.menu.enableContinue(false);
+	this.menu.node.style.width = display.width + "px";
+	this.menu.node.style.height = display.height * 2 + "px";
+	document.getElementById("snake-div").style.height = display.height + "px";
+	this.menu.saveButton.parentNode.addEventListener("submit", function (event) {
+		for(var i = 0; i < event.target.length; ++i){
+			var field = event.target[i];
+			if(field.name == "difficulty" && field.checked){
+				self.stepTime = Number(field.value);
+				if(self.timer) clearInterval(self.timer);
+				if(self.snake.alive) self.timer = setInterval(function () {
+					self.step();
+				}, self.stepTime);
+			}
+			else if(field.name == "walls") {
+				self.snake.throughWalls = field.checked;
+			}
+		}
+		self.menu.toggleSettings();
+		event.preventDefault();
+
+	});
+
+	this.timer = null;
+	this.enableKeys();
+
+}
+Game.prototype.enableKeys = function () {
+	var self = this; // lastTime = null;
+	document.addEventListener("keydown", function (event) {
+
+		if(event.keyCode == RIGHT && self.snake.direction != LEFT && self.wasStep){
+			self.snake.direction = RIGHT;
+			self.wasStep = false;
+			event.preventDefault();
+		}
+		else if(event.keyCode == LEFT && self.snake.direction != RIGHT && self.wasStep){
+			self.snake.direction = LEFT;
+			self.wasStep = false;
+			event.preventDefault();
+		}
+		else if(event.keyCode == UP && self.snake.direction != DOWN && self.wasStep){
+			self.snake.direction = UP;
+			self.wasStep = false;
+			event.preventDefault();
+		}
+		else if(event.keyCode == DOWN && self.snake.direction != UP && self.wasStep){
+			self.snake.direction = DOWN;
+			self.wasStep = false;
+			event.preventDefault();
+		}
+		else if(event.keyCode == 27) { //Escape button
+
+			if(self.menu.settings) {
+				self.menu.node.className = "settings_off";
+				self.menu.settings = false;
+			}
+
+			else if(self.snake.alive) {
+				// if (!self.lastTime) self.lastTime = Date.now();
+				if (Date.now() - self.lastTime >= 600 ) {
+					if (self.paused) {
+						var paused = self.menu.toggleView();
+						setTimeout(function () {
+							self.paused = paused;
+							self.menu.switchButtons(false);
+						}, 600);
+					}
+					else {
+						self.paused = self.menu.toggleView();
+						self.menu.switchButtons(true);
+					}
+					self.lastTime = Date.now();
+				}
+			}
+			event.preventDefault();
+		}
+
+	});
+};
+
+Game.prototype.run = function () {
+	var self = this;
+	if(this.snake.alive) {
+		this.timer = setInterval(function () {
+			self.step();
+		}, this.stepTime);
+	}
+	// else clearInterval(this.timer);
+};
+function run() {
+	snake.alive = true;
+	clearInterval(game.timer);
+	game.run();
+	game.menu.hide();
+}
+
+Game.prototype.step = function () {
+	if(this.snake.alive) {
+		if(!this.paused) {
+			this.snake.update();
+			this.display.draw(this.snake);
+			this.wasStep = true;
+		}
+	}
+	else {
+		//todo show dialog game over and name for highscores
+
+		clearInterval(this.timer);
+		this.menu.show();
+		this.menu.enableContinue(false);
+	}
+};
+
+Game.prototype.newGame = function () {
+
+	if(!game.snake.alive || window.confirm("Do you really want to leave? All progress will be lost.")) {
+		this.snake = new Snake();
+		this.display.firstDraw(this.snake);
+		this.snake.alive = true;
+		this.menu.hide();
+		this.paused = true;
+		clearInterval(this.timer);
+		var self = this;
+		this.timer = setInterval(function () {
+			self.step();
+		}, this.stepTime);
+		this.lastTime = Date.now();
+
+		setTimeout(function () {
+			self.paused = false;
+		}, 800);
+	}
+
+};
+
+Game.prototype.continue = function () {
+	this.paused = false;
+	this.menu.hide();
+};
+
+// var menu = new Menu();
+// var settings = new Menu("settings");
+
+
+var snake = new Snake();
+var canvas = document.getElementById("snake-canvas");
+var canvasDisplay = new CanvasDisplay(canvas);
+var game = new Game(snake, canvasDisplay);
+
+
+
+function slideDown() {
+	game.menu.toggleSettings();
+
+}
+function exit() {
+	console.log("gggg");
+	
+
+}
+
+
