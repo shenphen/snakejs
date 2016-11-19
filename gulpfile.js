b57b6e37c637f2bfea370c1b51ffeff01a4589d2
Karma Shenphen Dorje <adam.pol1234@gmail.com>
2016-11-19 11:58:14 +0100

    Added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var jshint = require('gulp-jshint');
+
+gulp.task('snake-js', function(){
+	gulp.src('snake.js')
+	.pipe(jshint())
+	.pipe(jshint.reporter('jshint-stylish'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
+})
+
+gulp.task('browserSync', function(){
+	browserSync({
+		server: {
+			baseDir:'./',
+		},
+		port: 6666
+
+	})
+})
+
+gulp.task('watch', ['browserSync', 'snake-js'], function(){
+	gulp.watch('snake.js', ['snake-js']);
+	gulp.watch('index.html', browserSync.reload);
+})
+
